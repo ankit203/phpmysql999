dc9d3cf2047718cc10701ca9a8edb78326b9d303
ankit203 <ankit.vohra@in.ibm.com>
2017-04-17 12:51:24 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,6 +2,8 @@
 
 function OpenCon()
  {
+	echo "Attempting DB connection.....";
+	/*
 	 if($vcap_services->{'cleardb'}){ //if cleardb mysql db service is bound to this application
         	 echo "cleardb bound. <br>";
         $db = $vcap_services->{'cleardb'}[0]->credentials;
@@ -10,15 +12,16 @@ function OpenCon()
         echo "Error: No suitable MySQL database bound to the application. <br>";
         //die();
     }
+	
     $mysql_database = $db->name;
     $mysql_port=$db->port;
     $mysql_server_name =$db->hostname . ':' . $db->port;
     $mysql_username = $db->username; 
     $mysql_password = $db->password;
-    
+    */
 $mysql_database = ad_2562c879fd43c90;
     $mysql_port=3306;
-    $mysql_server_name =$db->hostname . ':' . $db->3306;
+    $mysql_server_name =us-cdbr-iron-east-03.cleardb.net:3306;
     $mysql_username = bd5b4bd94d3c98; 
     $mysql_password = 4bc596d2;
 	
