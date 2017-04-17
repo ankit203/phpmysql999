f3e76449db27c5b6b4645e8258180d69b2e84e14
ankit203 <ankit.vohra@in.ibm.com>
2017-04-17 16:03:18 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,16 +2,15 @@
 
 function OpenCon()
  {
-	echo "Attempting DB connectionE.....<br>";
+	echo "Attempting DB connection.....<br>";
 	
 	$vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
 	 if($vcap_services->{'cleardb'}){ //if cleardb mysql db service is bound to this application
-        	 echo "cleardb bound. <br>";
         $db = $vcap_services->{'cleardb'}[0]->credentials;
     } 
     else { 
         echo "Error: No suitable MySQL database bound to the application. <br>";
-        //die();
+        die();
     }
 	
     $mysql_database = $db->name;
@@ -19,48 +18,23 @@ function OpenCon()
     $mysql_server_name =$db->hostname . ':' . $db->port;
     $mysql_username = $db->username; 
     $mysql_password = $db->password;
-   /*
-$mysql_database = ad_2562c879fd43c90;
-    $mysql_port=3306;
-    $mysql_server_name =us-cdbr-iron-east-03.cleardb.net:3306;
-    $mysql_username = bd5b4bd94d3c98; 
-    $mysql_password = 4bc596d2;
-	
-	*/
 	
 	if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    echo 'We don\'t have mysqli!!!\n';
+    echo 'We don\'t have mysqli!!!<br>';
 } else {
-    echo 'Phew, we have mysqli!\n';
+    echo 'Phew, we have mysqli!<br>';
 }
 	
     echo "Debug: " . $mysql_server_name . " " . $mysql_database . " " .  $mysql_username . " " .  $mysql_password . "\n";
-echo " \nConnecting.....  \n" ;
+echo " \nConnecting.....  <br>" ;
 $mysqli = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
-	echo " Checking......  \n" ;
+
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     die();
 }
-echo "Connected to MySQL kkkk.". "<br>" ;
+echo "Connected to MySQL.". "<br>" ;
 
-	/*
-$conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
-if ($conn->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
-    die();
-}
-echo "Connected to MySQL kkkk.". "<br>" ;
-*/
-/*
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "ankit";
- $db = "practice";
-
-
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-*/
  
  return $mysqli;
  }
