593c39490de3b7d5cf867b9513909e62ec2c27e0
ankit203 <ankit.vohra@in.ibm.com>
2017-04-17 13:00:07 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -3,7 +3,7 @@
 function OpenCon()
  {
 	echo "Attempting DB connection.....";
-	/*
+	
 	 if($vcap_services->{'cleardb'}){ //if cleardb mysql db service is bound to this application
         	 echo "cleardb bound. <br>";
         $db = $vcap_services->{'cleardb'}[0]->credentials;
@@ -18,12 +18,14 @@ function OpenCon()
     $mysql_server_name =$db->hostname . ':' . $db->port;
     $mysql_username = $db->username; 
     $mysql_password = $db->password;
-    */
+   /*
 $mysql_database = ad_2562c879fd43c90;
     $mysql_port=3306;
     $mysql_server_name =us-cdbr-iron-east-03.cleardb.net:3306;
     $mysql_username = bd5b4bd94d3c98; 
     $mysql_password = 4bc596d2;
+	
+	*/
 	
     echo "Debug: " . $mysql_server_name . " " .  $mysql_username . " " .  $mysql_password . "\n";
 
